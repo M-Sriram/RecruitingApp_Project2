785f22f6ed1b674b5e8c4a2d2cedba789fc5beed
M-Sriram <85308988+M-Sriram@users.noreply.github.com>
2024-04-17 23:22:30 -0400

    Home Screen Post Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
 import 'screens/candidates_screen.dart';
 import 'screens/create_post_screen.dart';
 import 'screens/job_listings_screen.dart';
-
+import 'models/post.dart';
+import 'screens/post_detail_screen.dart';
 
 void main() async {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final List<Post> posts = [
+    Post(title: 'Post 1', author: 'Author 1'),
+    Post(title: 'Post 2', author: 'Author 2'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,7 +25,7 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => HomeScreen(),
+        '/': (context) => HomeScreen(posts: posts),
         '/candidates': (context) => CandidatesScreen(),
         '/create_post': (context) => CreatePostScreen(),
         '/job_listings': (context) => JobListingsScreen(),
@@ -30,6 +35,10 @@ class MyApp extends StatelessWidget {
 }
 
 class HomeScreen extends StatelessWidget {
+  final List<Post> posts;
+
+  const HomeScreen({Key? key, required this.posts}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,7 +46,7 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: const Color(0xFF815854),
         title: Text('Recruiting Application'),
-        leading: Padding( // Logo in top left
+        leading: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Image.asset(
             'assets/app_logo.png',
@@ -49,25 +58,44 @@ class HomeScreen extends StatelessWidget {
           IconButton(
             icon: const Icon(Icons.menu),
             onPressed: () {
-
               _showMenu(context);
             },
           ),
         ],
       ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Welcome to the Recruiting Application!',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 20),
+          Text(
+            'Welcome to the Recruiting Application!',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
             ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 20),
+          Expanded(
+            child: ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: (context, index) {
+                final post = posts[index];
+                return ListTile(
+                  title: Text(post.title),
+                  subtitle: Text(post.author),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => PostDetailScreen(post: post),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -102,4 +130,3 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
-
